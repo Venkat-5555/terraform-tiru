c4f2953fbe1308a95e2c8b40234f0e25b593623f
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-02-28 12:25:33 +0530

    terraform vpc added

---
+++ terraform-aws-vpc/locals.tf
@@ -0,0 +1,5 @@
+locals   {
+    resource_name = "${var.project_name}-${var.enviornment}"
+    az_names = slice(data.aws_availability_zones.available.names,0,2)
+
+}
---
+++ terraform-aws-vpc/variables.tf
@@ -0,0 +1,98 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+variable "dns_hostnames" {
+    default = true
+}
+
+variable "project_name"  {
+    type = string
+}
+variable "enviornment"  {
+    type = string
+}
+
+variable common_tags {
+    default = {}
+}
+
+variable "vpc_tags"{
+    default = {}
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.public_subnet_cidrs) ==2
+      error_message = "please provide 2 subnets"
+    }
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.private_subnet_cidrs) ==2
+      error_message = "please provide 2 subnets"
+    }
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.database_subnet_cidrs) ==2
+      error_message = "please provide 2 subnets"
+    }
+}
+
+variable "pubilc_subnet_tags" {
+    default = {}
+  
+}
+
+variable "private_subnet_tags" {
+    default = {}
+  
+}
+
+variable "database_subnet_tags" {
+    default = {}
+  
+}
+
+variable "db_subnet_group_tags" {
+    default = {}
+  
+}
+variable "nat_gateway_tags" {
+    default = {}
+  
+}
+
+variable "public_route_table_tags" {
+    default = {}
+  
+}
+
+variable "private_route_table_tags" {
+    default = {}
+  
+}
+
+variable "database_route_table_tags" {
+    default = {}
+  
+}
+ 
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
+
